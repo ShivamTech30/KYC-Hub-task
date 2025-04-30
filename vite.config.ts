c1aa2b572b9c2bea29ff93f7156ad17d07a91c31
shivam <shivamsharma@externlabs.com>
2025-05-01 00:44:37 +0530

    ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'KYC-Hub-task',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
